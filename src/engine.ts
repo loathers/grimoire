ee1c0882e22c351d7fae62d54f7c2ba632dca60d
Kasekopf <jeffreydudek@gmail.com>
2022-07-30 17:32:34 -0700

    Reset lastEncounter before repeating from NC

--- src/engine.ts
+++ src/engine.ts
@@ -103,7 +103,10 @@ export class Engine<A extends string = never, T extends Task<A> = Task<A>> {
     for (const resource of task_resources.all()) resource.prepare?.();
     this.prepare(task);
     this.do(task);
-    while (this.shouldRepeatAdv(task)) this.do(task);
+    while (this.shouldRepeatAdv(task)) {
+      set("lastEncounter", "");
+      this.do(task);
+    }
     this.post(task);
 
     // Mark that we tried the task, and apply limits
