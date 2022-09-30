2662c41c42b62e0143e59221ab938994080ef96f
Jeffrey Dudek <jeffreydudek@gmail.com>
2022-09-29 18:21:12 -0700

    Allow for equipping duplicate accessories by specifying slots (#32)
    * Allow for equipping duplicate items on an outfit
    
    * Treat accessories as normaly equipment with upgrade options
    
    * Handle new accessory equipping
    
    * Remove equipAnother function for a simpler PR
    
    * Nit
    
    * Comment
    
    * No need to force accessories separately

--- src/outfit.ts
+++ src/outfit.ts
@@ -29,14 +29,13 @@ const weaponHands = (i?: Item) => (i ? mafiaWeaponHands(i) : 0);
 
 export class Outfit {
   equips: Map<Slot, Item> = new Map<Slot, Item>();
-  accessories: Item[] = [];
   skipDefaults = false;
   familiar?: Familiar;
   modifier = "";
   avoid: Item[] = [];
 
   private countEquipped(item: Item): number {
-    return [...this.equips.values(), ...this.accessories].filter((i) => i === item).length;
+    return [...this.equips.values()].filter((i) => i === item).length;
   }
 
   private isAvailable(item: Item): boolean {
@@ -48,7 +47,6 @@ export class Outfit {
 
   private haveEquipped(item: Item, slot?: Slot): boolean {
     if (slot === undefined) return this.countEquipped(item) > 0;
-    if ($slots`acc1, acc2, acc3`.includes(slot)) return this.accessories.includes(item); // TODO handle equipping multiple of an accessory
     return this.equips.get(slot) === item;
   }
 
@@ -81,9 +79,16 @@ export class Outfit {
   private equipAccessory(item: Item, slot?: Slot): boolean {
     if (![undefined, ...$slots`acc1, acc2, acc3`].includes(slot)) return false;
     if (toSlot(item) !== $slot`acc1`) return false;
-    if (this.accessories.length >= 3) return false;
     if (!canEquip(item)) return false;
-    this.accessories.push(item);
+    if (slot === undefined) {
+      // We don't care which of the accessory slots we equip in
+      const empty = $slots`acc1, acc2, acc3`.find((s) => !this.equips.has(s));
+      if (empty === undefined) return false;
+      this.equips.set(empty, item);
+    } else {
+      if (this.equips.has(slot)) return false;
+      this.equips.set(slot, item);
+    }
     return true;
   }
 
@@ -169,6 +174,25 @@ export class Outfit {
     return succeeded;
   }
 
+  /**
+   * Equip a thing to the outfit.
+   *
+   * If no slot is given, then the thing will be equipped wherever possible
+   * (possibly using dual-wielding, any of the accessory slots, or as
+   * familiar equipment). If it is impossible to add this thing anywhere to
+   * the outfit, this function will return false.
+   *
+   * If a slot is given, the item will be equipped only in that slot. If the
+   * slot is filled with a different item, this function will return false.
+   *
+   * If the thing is already equipped in the provided slot, or if no slot is
+   * given and the thing is already equipped in any slot, this function will
+   * return true and not change the outfit.
+   *
+   * @param thing The thing or things to equip.
+   * @param slot The slot to equip them.
+   * @returns True if the thing was sucessfully equipped, and false otherwise.
+   */
   equip(thing: Item | Familiar | OutfitSpec | Item[], slot?: Slot): boolean {
     if (Array.isArray(thing)) {
       if (slot !== undefined) return thing.some((val) => this.equip(val, slot));
@@ -179,9 +203,18 @@ export class Outfit {
     return this.equipSpec(thing);
   }
 
-  canEquip(thing: Item | Familiar | OutfitSpec | Item[]): boolean {
+  /**
+   * Check if it is possible to equip a thing to this outfit using .equip().
+   *
+   * This does not change the current outfit.
+   *
+   * @param thing The thing to equip.
+   * @param slot The slot to equip them.
+   * @returns True if this thing can be equipped.
+   */
+  canEquip(thing: Item | Familiar | OutfitSpec | Item[], slot?: Slot): boolean {
     const outfit = this.clone();
-    return outfit.equip(thing);
+    return outfit.equip(thing, slot);
   }
 
   /**
@@ -191,8 +224,15 @@ export class Outfit {
   dress(extraOptions?: Partial<MaximizeOptions>): void {
     if (this.familiar) useFamiliar(this.familiar);
     const targetEquipment = Array.from(this.equips.values());
-    const accessorySlots = $slots`acc1, acc2, acc3`;
-    for (const slot of $slots`weapon, off-hand, hat, shirt, pants, familiar, buddy-bjorn, crown-of-thrones, back`) {
+    const usedSlots = new Set<Slot>();
+
+    // First, we equip non-accessory equipment.
+    const nonaccessorySlots = $slots`weapon, off-hand, hat, back, shirt, pants, familiar, buddy-bjorn, crown-of-thrones`;
+
+    // We must manually remove equipment that we want to use in a different
+    // slot than where it is currently equipped, to avoid a mafia issue.
+    // Order is anchored here to prevent DFSS shenanigans
+    for (const slot of nonaccessorySlots) {
       if (
         (targetEquipment.includes(equippedItem(slot)) &&
           this.equips.get(slot) !== equippedItem(slot)) ||
@@ -201,36 +241,53 @@ export class Outfit {
         equip(slot, $item.none);
     }
 
-    //Order is anchored here to prevent DFSS shenanigans
-    for (const slot of $slots`weapon, off-hand, hat, back, shirt, pants, familiar, buddy-bjorn, crown-of-thrones`) {
+    // Then we equip all the non-accessory equipment.
+    for (const slot of nonaccessorySlots) {
       const equipment = this.equips.get(slot);
-      if (equipment) equip(slot, equipment);
-    }
-
-    //We don't care what order accessories are equipped in, just that they're equipped
-    const accessoryEquips = this.accessories;
-    for (const slot of accessorySlots) {
-      const toEquip = accessoryEquips.find(
-        (equip) =>
-          equippedAmount(equip) < accessoryEquips.filter((accessory) => accessory === equip).length
-      );
-      if (!toEquip) break;
-      const currentEquip = equippedItem(slot);
-      //We never want an empty accessory slot
-      if (
-        currentEquip === $item.none ||
-        equippedAmount(currentEquip) >
-          accessoryEquips.filter((accessory) => accessory === currentEquip).length
-      ) {
-        equip(slot, toEquip);
+      if (equipment) {
+        equip(slot, equipment);
+        usedSlots.add(slot);
       }
     }
 
+    // Next, we equip accessories
+    const accessorySlots = $slots`acc1, acc2, acc3`;
+    const accessoryEquips = accessorySlots
+      .map((slot) => this.equips.get(slot))
+      .filter((item) => item !== undefined) as Item[];
+
+    // To plan how to equip accessories, first check which accessories are
+    // already equipped in some accessory slot. There is no need to move them,
+    // since KoL doesn't care what order accessories are equipped in.
+    const missingAccessories = []; // accessories that are not already equipped
+    for (const accessory of accessoryEquips) {
+      const alreadyEquipped = accessorySlots.find(
+        (slot) => !usedSlots.has(slot) && equippedItem(slot) === accessory
+      );
+      if (alreadyEquipped) {
+        usedSlots.add(alreadyEquipped);
+      } else {
+        missingAccessories.push(accessory);
+      }
+    }
+
+    // Then, for all accessories that are not currently equipped, use the first
+    // open slot to place them.
+    for (const accessory of missingAccessories) {
+      const unusedSlot = accessorySlots.find((slot) => !usedSlots.has(slot));
+      if (unusedSlot === undefined) {
+        // This should only occur if there is a bug in .dress()
+        throw `No accessory slots remaining`;
+      }
+      equip(unusedSlot, accessory);
+      usedSlots.add(unusedSlot);
+    }
+
+    // Remaining slots are filled by the maximizer
     if (this.modifier) {
       const allRequirements = [
         new Requirement([this.modifier], {
-          preventSlot: [...this.equips.keys()],
-          forceEquip: accessoryEquips,
+          preventSlot: [...usedSlots],
           preventEquip: this.avoid,
         }),
       ];
@@ -245,13 +302,13 @@ export class Outfit {
     // Verify that all equipment was indeed equipped
     if (this.familiar !== undefined && myFamiliar() !== this.familiar)
       throw `Failed to fully dress (expected: familiar ${this.familiar})`;
-    for (const slotted_item of this.equips) {
-      if (equippedItem(slotted_item[0]) !== slotted_item[1]) {
-        throw `Failed to fully dress (expected: ${slotted_item[0]} ${slotted_item[1]})`;
+    for (const slot of nonaccessorySlots) {
+      if (this.equips.has(slot) && equippedItem(slot) !== this.equips.get(slot)) {
+        throw `Failed to fully dress (expected: ${slot} ${this.equips.get(slot)})`;
       }
     }
-    for (const accessory of this.accessories) {
-      if (!$slots`acc1, acc2, acc3`.some((slot) => equippedItem(slot) === accessory)) {
+    for (const accessory of accessoryEquips) {
+      if (equippedAmount(accessory) < accessoryEquips.filter((acc) => acc === accessory).length) {
         throw `Failed to fully dress (expected: acc ${accessory})`;
       }
     }
@@ -260,7 +317,6 @@ export class Outfit {
   clone(): Outfit {
     const result = new Outfit();
     result.equips = new Map(this.equips);
-    result.accessories = [...this.accessories];
     result.skipDefaults = this.skipDefaults;
     result.familiar = this.familiar;
     result.modifier = this.modifier;
