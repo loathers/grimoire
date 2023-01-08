e32af6df23e7aa9273ad9ab9b2063858658606fd
Kasekopf <jeffreydudek@gmail.com>
2023-01-08 13:10:04 -0800

    Parse values from settings in Args.fill instead of Args.create

--- src/args.ts
+++ src/args.ts
@@ -493,19 +493,9 @@ export class Args {
       [optionsSymbol]: options ?? {},
     } as any;
 
-    const metadata = Args.getMetadata(res);
-
-    // Parse values from settings.
-    metadata.traverseAndMaybeSet(res, (keySpec, key) => {
-      const setting: string = keySpec.setting ?? `${scriptName}_${keySpec.key ?? key}`;
-      if (setting === "") return undefined; // no setting
-      const value_str = getProperty(setting);
-      if (value_str === "") return undefined; // no setting
-      return parseAndValidate(keySpec, `Setting ${setting}`, value_str);
-    });
-
     if (options?.positionalArgs) {
       const keys: string[] = [];
+      const metadata = Args.getMetadata(res);
       metadata.traverse((keySpec, key) => {
         keys.push(keySpec.key ?? key);
       });
@@ -520,8 +510,13 @@ export class Args {
    * Parse the command line input into the provided script arguments.
    * @param args An object to hold the parsed argument values, from Args.create(*).
    * @param command The command line input.
+   * @param includeSettings If true, parse values from settings as well.
    */
-  static fill<T extends ArgMap>(args: ParsedArgs<T>, command: string | undefined): void {
+  static fill<T extends ArgMap>(
+    args: ParsedArgs<T>,
+    command: string | undefined,
+    includeSettings = true
+  ): void {
     if (command === undefined || command === "") return;
 
     const metadata = Args.getMetadata(args);
@@ -535,6 +530,17 @@ export class Args {
       if (keySpec.valueHelpName === "FLAG") flags.add(name);
       else keys.add(name);
     });
+
+    // Parse values from settings.
+    if (includeSettings) {
+      metadata.traverseAndMaybeSet(args, (keySpec, key) => {
+        const setting: string = keySpec.setting ?? `${metadata.scriptName}_${keySpec.key ?? key}`;
+        if (setting === "") return undefined; // no setting
+        const value_str = getProperty(setting);
+        if (value_str === "") return undefined; // no setting
+        return parseAndValidate(keySpec, `Setting ${setting}`, value_str);
+      });
+    }
 
     // Parse new argments from the command line
     const parsed = new CommandParser(
