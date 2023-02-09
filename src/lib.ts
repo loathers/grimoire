a3976534aa0c3bf116eb98b2b97a628118daef2f
horrible little slime <69secret69email69@gmail.com>
2023-02-09 17:21:26 -0500

    Fix type distribution

--- src/lib.ts
+++ src/lib.ts
@@ -1,4 +1,4 @@
-export type Delayed<T> = T extends () => unknown ? never : T | (() => T);
+export type Delayed<T> = [T] extends [() => unknown] ? never : T | (() => T);
 export function undelay<T>(delayedObject: Delayed<T>): T {
   return typeof delayedObject === "function" ? delayedObject() : delayedObject;
 }
